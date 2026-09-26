1a3b488e5c6e42c72641b7362886a9ded7807ee0
Elliria <Elliria@users.noreply.github.com>
2026-09-26 13:46:02 -0400

    Enhance Sphinx configuration with asset paths and comment cleanup
    * Change some wording in existing comments for clarity.
    * Add paths for the static and templates directories.
    * Add configuration for the master document.

--- docs-sphinx/conf.py
+++ docs-sphinx/conf.py
@@ -1,18 +1,24 @@
 import os
 import sys
 
-# Inject the parent directory into Python's active system path-matrix
+# Inject the parent directory into Python's active system path-matrix:
 # with '..' telling Python to step up into the repository root:
 sys.path.insert(0, os.path.abspath('..'))
 
-# --- Your standard Sphinx configurations continue below ---
+# --- Standard Sphinx configurations continue below ---
 project = 'Sphinx Sandbox: Code and Docs'
 copyright = '2026, Elliria'
 author = 'Elliria'
 
+# Provide the location for the theme assets in the resources sub-directory:
+templates_path = ['resources/templates']
+html_static_path = ['resources/static']
 extensions = [
     'sphinx.ext.autodoc',  # The engine that executes the code to grab docstrings
     'sphinx.ext.viewcode', # Adds handy "[source]" links to your compiled site
 ]
+
+# Provide the location for the master document:
+master_doc = 'index'
 
 html_theme = 'sphinx_rtd_theme'
